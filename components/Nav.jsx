15a20c3d4d1a7a4af9a53fdfbe477d4f189f7b5d
Ahmed Sabri <asabri@e3r8p15.1337.ma>
2024-08-11 19:19:19 +0100

    [UPDATE] : i just setup the project and i created the components

---
+++ components/Nav.jsx
@@ -0,0 +1,47 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
+const links = [
+    {
+        name: "Home",
+        Path: "/",
+    },
+    {
+        name: "Services",
+        Path: "/services",
+    },
+    {
+        name: "resume",
+        Path: "/resume",
+    },
+    {
+        name: "work",
+        Path: "/work",
+    },
+    {
+        name: "contact",
+        Path: "/contact",
+    },
+
+];
+const Nav = () => {
+
+const pathname = usePathname();
+
+return (
+    <nav className="flex gap-8">
+        {links.map((link,index )=>{
+            return (
+                <Link href={link.Path} key={index} className={ `${link.Path === pathname && "text-accent border-b-2 border-accent"
+                    } capitalize font-medium hover:text-accent transition-all`} >
+                    {link.name}
+                </Link>
+            )
+        })}
+    </nav>
+)
+}
+
+export default Nav
---
+++ components/Stairs.jsx
@@ -0,0 +1,30 @@
+import { motion } from "framer-motion"
+
+
+// variants
+const StairsAnimation = {
+    initial: {
+        top: "0%",
+    },
+    animate: {
+        top: "100%",
+    },
+    exit: {
+        top: ["100%", "0%"],
+    },
+}
+const Stairs = () => {
+return
+    <>
+=       {/* render 6 motion divs, each representing a step of stairs.
+
+        Each div will have the same animition defined by the stairsAnimation object. 
+        the delay for each div is calculated 
+        
+        
+        */}
+
+    </>
+}
+
+export default Stairs
